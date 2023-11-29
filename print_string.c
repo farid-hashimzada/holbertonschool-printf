28b0100b212fdd0304ec3c3db02fdf4750188bfd
Murad Abbaszade <muradabbaszade6@gmail.com>
2023-11-29 11:35:09 -0800

    a

--- print_string.c
+++ print_string.c
@@ -1,2 +1,12 @@
 #include <stdio.h>
 #include "main.h"
+/**
+ * print_string - print string
+ *  @c: char
+ */
+void print_string(char *c){
+    while(*c){
+        print_char(*c);
+        c++;
+    }
+}
