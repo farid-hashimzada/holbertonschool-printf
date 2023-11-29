2d4a1b705a0d1ce8d8eb003a9827918c98701fe0
Murad Abbaszade <muradabbaszade6@gmail.com>
2023-11-29 11:44:01 -0800

    a

--- printf_.c
+++ printf_.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdarg.h>
-#include <string.h>
+/**
+ * printf_ - print 
+ *  @format: format
+ * Result - length of the output
+ */
 int printf_(char *format, ...)
 {
 
