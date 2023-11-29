e4a90f5afca1cbc88269cde5504228a5800f3c90
Murad Abbaszade <muradabbaszade6@gmail.com>
2023-11-29 11:49:32 -0800

    a

--- printf_.c
+++ printf_.c
@@ -12,4 +12,28 @@ int printf_(char *format, ...)
     va_start(args,format);
     if (format == NULL)
     exit(98);
+    while(*format){
+        if(*format == '%'){
+            format++;
+            if(*format == 'c'){
+                print_char(va_arg(args,char*));
+                length++;
+            }
+            else if(*format == 's'){
+                print_string(va_arg(args,char*));
+                length++;
+            }
+            else{
+                print_char('%');
+                print_char(*format);
+                length+=2;
+            }
+        }
+        else{
+            print_char(*format);
+            length++;
+        }
+        format++;
+    }
+    return (length);
 }
