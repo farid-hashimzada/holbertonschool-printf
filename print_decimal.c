3f810811a4a2ed03fe1a753fb4f6fe5027dca9a6
abbaszade <muradabbaszade6@gmail.com>
2023-11-30 19:07:03 +0400

    Update print_decimal.c

--- print_decimal.c
+++ print_decimal.c
@@ -14,20 +14,22 @@ int b = a;
 char c;
 if(a<0){
     char negative = '-';
-    result += write(1, &negative, 1);
+    write(1, &negative, 1);
     a = a * (-1);
     b = b * (-1);
+    result++;
 }
 while ((b / i) >= 10)
 {
 i *= 10;
 len++;
 }
+result+=len;
 while(len>0){
     b = a/i;
     b = b%10;
     c = b + 48;
-    result+=write(1, &c, 1);
+    write(1, &c, 1);
     i=i/10;
     len--;
 }
