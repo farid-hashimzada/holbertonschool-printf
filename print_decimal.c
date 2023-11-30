cd8c946a2c971b722721ae95e94494c4f2332f24
abbaszade <muradabbaszade6@gmail.com>
2023-11-30 16:52:25 +0400

    Create print_decimal.c

---
+++ print_decimal.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <main.h>
+/**
+ * print_decimal - print decimal
+ * @a: a
+ */
+void print_decimal(int a){
+int i = 1;
+int b = a;
+while ((b / i) > 10)
+{
+i *= 10;
+}
+while (a > 1)
+{
+b = a / i;
+a = a % i;
+print_char(b + 48);
+i /= 10;
+}
+}
