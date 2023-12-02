195f673b9819cf768229670591057f1d4e9a50d8
Farid-Hashimzade <124304902+farid-hashimzada@users.noreply.github.com>
2023-12-02 19:08:44 +0400

    Update print_decimal.c

--- print_decimal.c
+++ print_decimal.c
@@ -1,30 +1,41 @@
 #include <stdarg.h>
 #include "main.h"
 #include <unistd.h>
-
 /**
  * print_decimal - Prints a decimal
- * @a: parameter
- * Return: number of characters printed
+ * @a: parametr
+ * Return: result
  */
 int print_decimal(int a)
 {
-    int result = 0;
+	int i = 1, len = 1, result = 0;
+	unsigned int b = a;
+	unsigned int d = a;
+	char c;
 
-    if (a < 0)
-    {
-        write(1, "-", 1);
-        result++;
-        a = -a;
-    }
+if (a < 0)
+{
+	char negative = '-';
 
-    int i;
-
-    if (a / 10)
-        result += print_decimal(a / 10);
-
-    char c = a % 10 + '0';
-    write(1, &c, 1);
-
-    return (result + 1);
+	write(1, &negative, 1);
+	d = d * (-1);
+	b = b * (-1);
+	result++;
+}
+while ((b / i) >= 10)
+{
+	i *= 10;
+	len++;
+}
+	result += len;
+while (len > 0)
+{
+	b = d / i;
+	b = b % 10;
+	c = b + 48;
+	write(1, &c, 1);
+	i = i / 10;
+	len--;
+}
+	return (result-1);
 }
