4d32c047783e2d1e234ca86f6d22682c1b84bdf0
abbaszade <muradabbaszade6@gmail.com>
2023-12-02 14:56:59 +0400

    Update print_decimal.c

--- print_decimal.c
+++ print_decimal.c
@@ -8,12 +8,14 @@
  * @ptr: Pointer to the va_list containing the string
  * @len: Pointer to the length counter
  */
-int print_decimal(int a){
-int i = 1, len = 1,result=0;
+int print_decimal(int a)
+{
+int i = 1, len = 1, result = 0;
 unsigned int b = a;
 unsigned int d = a;
 char c;
-if(a<0){
+if (a < 0)
+{
     char negative = '-';
     write(1, &negative, 1);
     d = d * (-1);
@@ -25,14 +27,15 @@ while ((b / i) >= 10)
 i *= 10;
 len++;
 }
-result+=len;
-while(len>0){
-    b = d/i;
-    b = b%10;
+result += len;
+while (len > 0)
+{
+    b = d / i;
+    b = b % 10;
     c = b + 48;
     write(1, &c, 1);
-    i=i/10;
+    i = i / 10;
     len--;
 }
-return (result-1);
+return (result - 1);
 }
