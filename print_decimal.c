ecf072f80be683e653b144d7891a6213e4460caf
abbaszade <muradabbaszade6@gmail.com>
2023-12-03 15:16:04 +0400

    Update print_decimal.c

--- print_decimal.c
+++ print_decimal.c
@@ -3,39 +3,38 @@
 #include <unistd.h>
 /**
  * print_decimal - Prints a decimal
- * @a: parametr
- * Return: result
+ * @a: Pointer to the va_list containing the string
+ * Return:len
  */
 int print_decimal(int a)
 {
-	int i = 1, len = 1, result = 0;
-	unsigned int b = a;
-	unsigned int d = a;
-	char c;
+int i = 1, len = 1, result = 0;
+unsigned int b = a;
+unsigned int d = a;
+char c;
 
 if (a < 0)
 {
-	char negative = '-';
-
-	write(1, &negative, 1);
-	d = d * (-1);
-	b = b * (-1);
-	result ++;
+char negative = '-';
+write(1, &negative, 1);
+d = d * (-1);
+b = b * (-1);
+result++;
 }
 while ((b / i) >= 10)
 {
-	i *= 10;
-	len ++;
+i *= 10;
+len++;
 }
-	result += len;
+result += len;
 while (len > 0)
 {
-	b = d / i;
-	b = b % 10;
-	c = b + 48;
-	write(1, &c, 1);
-	i = i / 10;
-	len--;
+b = d / i;
+b = b % 10;
+c = b + 48;
+write(1, &c, 1);
+i = i / 10;
+len--;
 }
-	return (result-1);
+return (result - 1);
 }
